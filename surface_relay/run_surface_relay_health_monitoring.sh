c7ae4fc5c4c33fbf016a2b69cdf6bf52d2bd121e
Grant Blackwell <gblacky349@gmail.com>
2023-09-11 11:22:43 -0700

    updated run... bash script to also be the config script on first run

--- surface_relay/run_surface_relay_health_monitoring.sh
+++ surface_relay/run_surface_relay_health_monitoring.sh
@@ -2,12 +2,37 @@
 
 # Purpose of this script is to start the Surface Relay health monitoring system script file
 
-# Check if /data_storage/ folder exists. If it doesn't, create it
-if ! [ -d "data_storage_sensors/" ]
+# Check if config_status file exists. If it doesn't, create it and the rest of the config setup 
+if ! [ -d "config_status.txt" ]
 then
+	# Create config_status.txt
+	echo "Creating config_status.txt"
+	touch config_status.txt
+	echo 'config_status.txt created' | sudo tee -a config_status.txt
+	echo ""
+
+	# Create the data_storage_sensors folder
 	echo "Creating data_storage_sensors folder"
 	mkdir data_storage_sensors
+	echo '/data_storage_sensors/ folder created' | sudo tee -a config_status.txt
+	echo ""
+
+	# Move the libphidget libraries to the correct location
+	echo "Moving the Phidget libraries to the proper location"
+	cd ../phidget_libraries/
+	cp -R libphidget22-1.16.20230707 /usr/lib/
+	cp -R libphidget22extra-1.16.20230707 /usr/lib/
+	echo 'Phidget libraries moved to proper location' | sudo tee -a config_status.txt
+	echo ""
 fi
+
+# Check if /data_storage/ folder exists. If it doesn't, create it
+#if ! [ -d "data_storage_sensors/" ]
+#then
+#	echo "Creating data_storage_sensors folder"
+#	mkdir data_storage_sensors
+#	echo '/data_storage_sensors/ folder created' | sudo tee -a config_status.txt
+#fi
 
 # Let user know the script is starting
 echo
